31e54092e55a9ddb8c9e1ed1670b05d4e7bb5a8f
Thomas <thomas@gmail.com>
2024-12-08 17:32:06 +0000

    bind to all networks

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port,0.0.0.0, () => {
     console.log(`Server is running on http://ec2-34-204-45-84.compute-1.amazonaws.com:${port}`);
 });
